3274b5ee6405301b3f5e18766cf69d590b0ff262
budosen <panjidia995@gmail.com>
2019-10-06 16:34:03 +0700

    menambahkan tugas ke 5 PWL - Simple Timezone Selector + unittest etc.

--- application/controllers/Pertemuansepi.php
+++ application/controllers/Pertemuansepi.php
@@ -5,7 +5,7 @@ class Pertemuansepi extends CI_Controller {
 
     public function index($region='Asia',$city='Jakarta'){
         $this->benchmark->mark('code_start');
-        $this->load->helper(array('form', 'url','date'));
+        $this->load->helper(array('form', 'url','date','language'));
 
         $default = $region."/".$city;
         $format = 'DATE_RFC822';
@@ -26,21 +26,63 @@ class Pertemuansepi extends CI_Controller {
 
     public function selectReg($region='Asia',$city='Jakarta'){
         $this->benchmark->mark('code_start');
-        $this->load->helper(array('form', 'url','date','html','language','cookie'));
+        $this->load->helper(array('form','url','date','html'));
+        $this->load->library(array('unit_test', 'table'));
 
         $default = $region."/".$city;
 
-        set_cookie('simpan',$default,99999999);
         $format = 'DATE_RFC822';
-        $time = now($default);
-        $time = standard_date($format, $time);
+        $time1 = now($default);
+        $time = standard_date($format, $time1);
+
+        $test = $time;
+        $expected_result = standard_date($format, $time1);
+        $test_name = 'test correct date';
+        $notes = "inputan region & kota dibandingkan di test ini";
+        
+        $this->unit->run($test, $expected_result, $test_name,$notes);
+
+        // $aa = $this->unit->result();
+
+        $template = array(
+            'table_open'            => '<table border="1" cellpadding="4" cellspacing="0">',
+        
+            'thead_open'            => '<thead>',
+            'thead_close'           => '</thead>',
+        
+            'heading_row_start'     => '<tr>',
+            'heading_row_end'       => '</tr>',
+            'heading_cell_start'    => '<th>',
+            'heading_cell_end'      => '</th>',
+        
+            'tbody_open'            => '<tbody>',
+            'tbody_close'           => '</tbody>',
+        
+            'row_start'             => '<tr>',
+            'row_end'               => '</tr>',
+            'cell_start'            => '<td>',
+            'cell_end'              => '</td>',
+        
+            'row_alt_start'         => '<tr>',
+            'row_alt_end'           => '</tr>',
+            'cell_alt_start'        => '<td>',
+            'cell_alt_end'          => '</td>',
+        
+            'table_close'           => '</table>'
+        );
+        
+        $this->table->set_template($template);
         $this->benchmark->mark('code_end');
+
         $eta = $this->benchmark->elapsed_time('code_start', 'code_end');
         $mem = $this->benchmark->memory_usage();
+     
         $data = array(
             'memo'=>$mem,
             'time'=>$time,
-            'eta'=>$eta
+            'def'=>$default,
+            'eta'=>$eta,
+            // 'unitTest'=>$aa
         );
 
         $this->load->view('tgl',$data);
